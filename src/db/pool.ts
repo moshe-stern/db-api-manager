48d318f7e48d6020fc3f1b1541af2b140dc40752
moshe-stern <guntheramassed@gmail.com>
2024-12-05 16:17:44 -0500

    health check for pools

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -6,15 +6,18 @@ enum EDbNames {
   attainDMAttainAutomation = 'AttainDM.AttainAutomation'
 }
 
-let pool: {[key in EDbNames]: sql.ConnectionPool | null} = {
+const pool: {
+  [key in EDbNames]: sql.ConnectionPool | null} = {
   [EDbNames.attainDataLake]: null,
   [EDbNames.attainDMAttainAutomation]: null
 };
 
 async function getPool(dbName: EDbNames): Promise<sql.ConnectionPool> {
-  if (pool[dbName]) {
+  
+  if (pool[dbName] && await isHealthy(pool[dbName])) {
     return pool[dbName]
   }
+
   const credential = new DefaultAzureCredential();
   const tokenResponse = await credential.getToken(
     "https://database.windows.net/.default",
@@ -38,6 +41,18 @@ async function getPool(dbName: EDbNames): Promise<sql.ConnectionPool> {
   await newPool.connect();
   pool[dbName] = newPool;
   return pool[dbName];
+}
+
+
+async function isHealthy(pool:sql.ConnectionPool) {
+  try {
+    const res = await pool.query('SELECT 1')
+    return res.recordsets.length
+  } catch (error) {
+    console.error(error)
+    console.log('Creating new Pool')
+    return null
+  }
 }
 
 export {
