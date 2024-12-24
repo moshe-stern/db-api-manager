1fccc3f1dcb228e8e80a203783d5288716228a35
moshe-stern <guntheramassed@gmail.com>
2024-12-24 12:20:24 -0500

    try again

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 8080;
 
 app.use(express.json());
-// app.use("/auth", authRoute);
+app.use("/auth", authRoute);
 app.use("/cube-status", cubeStatus);
 app.use(auth);
 app.use("/twilio", twilio);
