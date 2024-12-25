56ae0af459b7b857e1329dec08ca159bcb0c2478
moshe-stern <guntheramassed@gmail.com>
2024-12-25 09:43:37 -0500

    auth

---
+++ src/helpers/index.ts
@@ -0,0 +1,27 @@
+import { AppError } from "attain-aba-shared";
+import { Response, Request, NextFunction } from "express";
+async function errorHandler(err: AppError, req: Request, res: Response, next: NextFunction) {
+    const statusCode = err.status || 500;
+    const errorMessage = err.isOperational
+        ? err.message
+        : "An unknown error occured";
+    console.error("Error:", err);
+    res.status(statusCode).json({
+        error: {
+            message: errorMessage,
+        },
+    });
+}
+function auth(req: Request, res: Response, next: NextFunction) {
+    const secretKey = req.header('SECRET_KEY');
+    const expectedKey = process.env.SECRET_KEY;
+    if (!secretKey || secretKey !== expectedKey) {
+        res.status(403)
+        return
+    }
+    next();
+}
+
+export {
+    errorHandler, auth
+}
